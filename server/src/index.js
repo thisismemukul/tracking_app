95cbda1dcecc42db2828518b9bf293caa9e02f76
Mukul Saini <thisismemukul@gmail.com>
2022-07-19 16:10:56 +0530

    Connecting mongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const mongoURI = 'mongodb+srv://thisismemukul:0VYPbULMpfMr1fb6@cluster0.myrvduq.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to mongo', err);
+});
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
